ad95b8eb14ccdec72fe08a4c5205570ada6b4ee8
Supidan <31026995+Supidan@users.noreply.github.com>
2020-07-14 17:21:13 +0700

    perapihan login

--- app/src/main/java/com/siren/docuved/ui/activity/Login.kt
+++ app/src/main/java/com/siren/docuved/ui/activity/Login.kt
@@ -11,35 +11,35 @@ import com.siren.docuved.R
 import com.siren.docuved.base.BaseActivity
 
 @SuppressLint("Registered")
-class Login : BaseActivity(), View.OnClickListener {
+class Login : BaseActivity() {
     var email: EditText? = null
     var password: EditText? = null
     var lupa: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        email = findViewById<View>(R.id.email) as EditText
-        password = findViewById<View>(R.id.password) as EditText
-        val btn = findViewById<View>(R.id.button1) as Button
-        btn.setOnClickListener(this)
+//        email = findViewById<View>(R.id.email) as EditText
+//        password = findViewById<View>(R.id.password) as EditText
+//        val btn = findViewById<View>(R.id.button1) as Button
+//        btn.setOnClickListener(this)
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.button1) {
-            firebaseRef.collection("user")
-                .whereEqualTo("email", email)
-                .get()
-                .addOnSuccessListener { result ->
-                    Log.d("result", result.toString())
-                    for (document in result) {
-                        Log.d("test", "${document.id} => ${document.data}")
-                    }
-                }
-                .addOnFailureListener() { exception ->
-                    Log.w("err", "error getting documents.", exception)
-                }
-            val i = Intent(this, DashboardSiswa::class.java)
-            startActivity(i)
-        }
-    }
+//    override fun onClick(v: View) {
+//        if (v.id == R.id.button1) {
+//            firebaseRef.collection("user")
+//                .whereEqualTo("email", email)
+//                .get()
+//                .addOnSuccessListener { result ->
+//                    Log.d("result", result.toString())
+//                    for (document in result) {
+//                        Log.d("test", "${document.id} => ${document.data}")
+//                    }
+//                }
+//                .addOnFailureListener() { exception ->
+//                    Log.w("err", "error getting documents.", exception)
+//                }
+//            val i = Intent(this, DashboardSiswa::class.java)
+//            startActivity(i)
+//        }
+//    }
 }
